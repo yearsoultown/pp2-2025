56e38516368608da9c6a91c931f6acf16f5a9e39
Makishev Yersultan <makishevyersultan06@gmail.com>
2025-01-18 16:18:54 +0500

    intro for python folder what is python?

---
+++ week1/intro.py
@@ -0,0 +1,1 @@
+print("What is Python? Python is a popular programming language. It was created by Guido van Rossum, and released in 1991. It is used for:web development (server-side), software development, mathematics, system scripting.")
