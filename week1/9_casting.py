114258891751b07299cef85673207f280c84a9c4
Makishev Yersultan <makishevyersultan06@gmail.com>
2025-01-18 17:16:53 +0500

    casting - changing the type of variables' values, in other words - customing

---
+++ week1/9_casting.py
@@ -0,0 +1,14 @@
+x = int(5) #integer
+y = int(5.6) #converts to integer (5)
+z0 = "6"
+z1 = int(z0) #converts str value of z0 "6" into integer (6)
+d = float(14) # will be 14.0
+z = float("30")   # z will be 30.0
+w = float("4.20") # w will be 4.2
+
+print(z)
+print(w)
+
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
