c654fe5ed3d370fe5a2906250752621f498bcb4e
Makishev Yersultan <makishevyersultan06@gmail.com>
2025-01-18 17:00:56 +0500

    variables exercises have been submitted

---
+++ week1/6_variables.py
@@ -0,0 +1,42 @@
+a = "me" # stands for string
+b = "you" # stands for string
+c = "and" # stands for string
+d = 2 # stands for integer
+e = '2' # stands for string
+
+print(a)
+print(c)
+print(b)
+print(d)
+print(e)
+
+# legal initializing of variables
+myvar = "Yersul"
+my_var = "Yersul"
+_my_var = "Yersul"
+myVar = "Yersul"
+MYVAR = "Yersul"
+myvar2 = "Yersul"
+
+# illegal initializing of variables
+"""
+2myvar = "Yersul"
+my-var = "Yersul"
+my var = "Yersul"
+"""
+
+# Many Values to Multiple Variables
+
+x, y, z = 1, 2, 12
+
+print(x + y + z)
+
+a = b = c = "Orange"
+
+print(a, b, c)
+
+fruits = ["banana", "apple", "grapes"]
+
+f, g, h = fruits
+
+print(f, g, h)
