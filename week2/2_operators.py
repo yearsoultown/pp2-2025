f848a516f2f3906275173b508a0256496b164f2e
Makishev Yersultan <makishevyersultan06@gmail.com>
2025-01-25 14:44:30 +0500

    operators topic

---
+++ week2/2_operators.py
@@ -0,0 +1,63 @@
+a = 100
+b = 5
+c = 17
+
+#addition (+)
+print(a+b+c)
+
+#subtraction (-)
+print(a-b-c)
+
+#multiplication (*)
+print(c * b)
+
+#division (/)
+print(a/b)
+print(a/c)
+
+#modulus (%)
+print(a % c)
+
+#exponentiation (**)
+print(a**b)
+
+#floor division (//)
+print(a//c)
+
+# we can use these instead of writing longer (assignment operators)
+a=b
+a %= b
+a += b
+a -=b
+a *= b
+a //= b
+a **= b
+a /= b
+
+#comparison operators
+a == b #equal
+a != b #unequal
+a > b #greater than
+a < b #smaller than
+a >= b #greater or equal
+a <= b #less or equal to
+
+#logical operators
+"and" - "Returns True if both statements are true"
+"or" - "Returns True if one of the statements is true"
+"not" - "Reverse the result, returns False if the result is true"
+
+#identity operators
+"is" - "Returns True if both variables are the same object"
+"is not" - "Returns True if both variables are not the same object"
+
+#membership operators
+"in" - "Returns True if a sequence with the specified value is present in the object"
+"not in" - "Returns True if a sequence with the specified value is not present in the objec"
+
+#bitwise operators
+"&" - "and"
+"|" - "or"
+"^" - "xor"
+"~" - "not"
+
