c6b54e04916d4440c05981d8ca48d7a1d57d68eb
Makishev Yersultan <makishevyersultan06@gmail.com>
2025-02-02 18:11:19 +0500

    functions2 is done

--- week3/2_functions2.py
+++ week3/2_functions2.py
@@ -78,6 +78,57 @@ movies = [
 }
 ]
 
+#Write a function that takes a single movie and returns True if its IMDB score is above 5.5
+
+#1st task
+def one_movie_above_five_dot_five_score(random_movie):
+    if random_movie["imdb"] > 5.5:
+        return True
+    else:
+        return False
+
+print("choose numbers from 0 to 14:")
+x = int(input())
+print(one_movie_above_five_dot_five_score(movies[x]))
+
+#2nd task
+def sublist_imdb_above_five_and_five(aboves):
+    sublist = []
+    for i in aboves:
+        if i["imdb"] < 5.5:
+            sublist.append(i)
+            
+    for i in sublist:
+        print(i)
+
+sublist_imdb_above_five_and_five(movies)
+
+#3rd task
+def category_depended_movies(overall, specified):
+    specified_category = []
+    for i in overall:
+        if i["category"] == specified:
+            specified_category.append(i["name"])
+    print(specified_category)
+
+print("choose a category of a movie:")
+ctgry = input()
+category_depended_movies(movies, ctgry)
+
+#4th task
+total = len(movies)
+
+def calculating_average(movies, total):
+    allsum = 0
+    for i in movies:
+        allsum += i["imdb"]
+    avg = allsum / total
+    
+    return avg
+
+print(calculating_average(movies, total))
+
+#5th task
 def ctg_imdb_avg(all_movies):
     category = input()
     total_of_category = 0
